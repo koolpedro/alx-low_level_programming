b6334dd274ff0f8c5431bb825a28150296259958
koolpedro <panyakorah@gmail.com>
2022-09-08 17:19:37 -0700

    task 4 hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- *main - program prints with function puts
- *   
- *Return: Always 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
