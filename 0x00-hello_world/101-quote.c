ad4af5c5037db84f65bf9db5b21ef8a4bb906aa0
koolpedro <panyakorah@gmail.com>
2022-09-08 18:29:20 -0700

    101 quotetask

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- *main - print size of data
- *
- *Return: Always 0
+ * main - Entry point
+ * Return: Always 1 (Success)
  */
+
 int main(void)
+
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+
+		write(STDOUT_FILENO, "and that piece of art is useful
+				\" - Dora Korpar, 2015-10-19\n", 59);
+
+			return (1);
 }
