f346c11bd27be458b1fd45cf2b749df923b1e459
koolpedro <panyakorah@gmail.com>
2022-09-12 07:56:43 -0700

    12. task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,19 +7,35 @@
  */
 int main(void)
 {
-	int c, i;
+	int c, i, k, j;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = 48; c <= 57; c++)
 	{
-		for (i = '0'; i <= '9'; i++)
+		for (i = 48; i <= 57; i++)
 		{
-			putchar(c);
-			putchar(i);
-
-			if ((c != '9')  || (c == '9' && i != '9'))
+			for (k = 48; k <= 57; k++)
 			{
-				putchar(',');
-				putchar(' ');
+				for (j = 48; j <= 57; j++)
+				{
+					if (((k + j) > (c + i) && K >= c)  || c < k)
+					{
+						putchar(c);
+						putchar(i);
+						putchar(' ');
+						putchar(k);
+						putchar(j);
+
+						if (c + i + k + j ==227 && c == 57)
+						{
+							break;
+						}
+						else
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
 			}
 		}
 	}
