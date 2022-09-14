c84694f62ce57fe283d988bede6dec7330d07987
koolpedro <panyakorah@gmail.com>
2022-09-13 17:51:51 -0700

    putcahr task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - print putchar
+ * Return: always 0
+ */
+
+int main(void)
+{
+	_putcahr('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putcahr('a');
+	_putchar('r');
+	_putcahr('\n');
+
+	return (0);
+}	
+
