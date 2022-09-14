c242020078143a1618a5b9f62dc7a7cab29d6669
koolpedro <panyakorah@gmail.com>
2022-09-14 05:21:54 -0700

    0.putchartask

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef main_h
 #define main_h
 
-int_putchar(char c);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -14,6 +14,6 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-void num(int r, init n);
+void num(int r, int n);
 
 #endif
