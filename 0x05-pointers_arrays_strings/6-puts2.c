bed4fafdf0500c6d10221d68cc71e0ede04002bf
koolpedro <panyakorah@gmail.com>
2022-09-19 16:01:47 -0700

    6.puts2 task

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,14 +7,16 @@
  *
  * Return: void
  */
+
 void puts2(char *str)
 {
 	int i = 0;
 
-	while (*(str + i))
+	while (*(str + i) != '\0')
 	{
-		putchar(*(str + i));
-		i = i + 2;
+		if (i % 2 == 0)
+			putchar(*(str + i));
+		i++;
 	}
-	putchar('\n');
+	putchar(10);
 }
