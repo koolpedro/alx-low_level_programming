f14bdda663e5c897b29ac25cb4fa832daeb96828
koolpedro <panyakorah@gmail.com>
2022-09-21 08:07:58 -0700

    0.strcat task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,7 +14,7 @@ char *strcat(char *dest, const char *src)
 		dest_len++;
 
 	for (index = 0; src[index]; index++)
-		dest[dest_len++} = src[index];
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
