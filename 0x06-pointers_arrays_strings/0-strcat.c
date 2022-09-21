b83d27126c8c23dfd850b8cc31af5505705be575
koolpedro <panyakorah@gmail.com>
2022-09-21 08:05:46 -0700

    0.strcat task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * strcat - this is a function strcat
+ * @desc: first param
+ * @src: second param
+ * Return: a string
+ */
+char *strcat(char *dest, const char *src)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++} = src[index];
+
+	return (dest);
+}
