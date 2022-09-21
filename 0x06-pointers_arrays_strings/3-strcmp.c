c5921c961426084bd7cd40d0e4295f24a900bdda
koolpedro <panyakorah@gmail.com>
2022-09-21 16:00:10 -0700

    3.strcmp task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,8 +6,8 @@
  * @s2: A pointer to the second string to be compared.
  *
  * Return: if str1 < str2, the negative difference of the first unmatched char
- * 	   if str1 == str2, 0.
- * 	   if str1 > str2, the positive difference of the first unmatched char
+ * if str1 == str2, 0.
+ * if str1 > str2, the positive difference of the first unmatched char
  */
 int_strcmp(char *s1, char *s2)
 {
