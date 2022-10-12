a27011b460b3e10cc177ea19c5d2838f0a6e3dd4
koolpedro <panyakorah@gmail.com>
2022-10-12 14:58:59 -0700

    printname

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,8 +9,11 @@
  *
  * Return: Nothing
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
