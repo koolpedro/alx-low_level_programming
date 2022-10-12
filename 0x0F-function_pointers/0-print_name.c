f4bcb3d174c833186104dc6bf9353311a3c490f0
koolpedro <panyakorah@gmail.com>
2022-10-12 14:42:41 -0700

    printname

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,6 @@
 #include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * print_name - writes the character c to stdout
@@ -9,8 +11,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	 if (name == NULL || f == NULL)
-		 return;
-
-	 f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
