b5e3bbb1d1bd3fd62a2cf7f2c422117045c9de08
koolpedro <panyakorah@gmail.com>
2022-10-12 14:15:38 -0700

    2.four.c task

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * *get_op_func - contain the function that selects the correct function
+ * @s: variable pointer
+ *
+ * Return: NULL
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i = 0;
+
+	while (i <= 4)
+	{
+		if (ops[i].op[0] == *s)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - my program in c
+ * @argc: int argument count
+ * @argv: char pointer
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	char *op;
+	char op2;
+	int a, b, res;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	op = argv[2];
+	op2 = *argv[2];
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if ((op2 != 43 && op2 != 45 && op2 != 37
+				 && op2 != 47 && op2 != 42) || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((op2 == 47 || op2 == 37) && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	res = (*get_op_func(op))(a, b);
+
+	printf("%d\n", res);
+
+
+	return (0);
+}
