b3e55c78d3bf707626940edb341cf6cfc4b3e8c4
koolpedro <panyakorah@gmail.com>
2022-10-13 16:03:35 -0700

    printall

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,87 +1,102 @@
-#include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+void print_int(va_list args);
+void print_char(va_list args);
+void print_string(va_list args);
+void print_double(va_list args);
 /**
- * tchar - prints variadic argument char
- * @list: variadic list
- *
- * Return: No return
+ * print_int - that prints integers
+ * @args: va_list
  */
-void tchar(va_list list)
+void print_int(va_list args)
 {
-	printf("%c", va_arg(list, int));
-}
-/**
- * tint - prints variadic argument int
- * @list: variadic list
- *
- * Return: No return
- */
-void tint(va_list list)
-{
-	printf("%i", va_arg(list, int));
-}
-/**
- * tfloat - prints variadic argument float
- * @list: variadic list
- *
- * Return: No return
- */
-void tfloat(va_list list)
-{
-	printf("%f", va_arg(list, double));
-}
-/**
- * tstring - prints variadic argument string
- * @list: variadic list
- *
- * Return: No return
- */
-void tstring(va_list list)
-{
-	char *tmp;
+	int i;
 
-	tmp = va_arg(list, char *);
-	if (tmp == 0)
-		tmp = "(nil)";
-	printf("%s", tmp);
+	i = va_arg(args, int);
+	printf("%d", i);
 }
+
 /**
- * print_all - prints anything
- * @format: list of types of arguments passed to the function
- * @...: Arguments Variadic
- *
- * Return: No return
+ * print_char - that prints characters
+ * @args: va_list
+ */
+void print_char(va_list args)
+{
+	int c;
+
+	c = va_arg(args, int);
+	printf("%c", c);
+}
+
+/**
+ * print_string - that prints strings
+ * @args: va_list
+ */
+void print_string(va_list args)
+{
+	char *str;
+
+	str = va_arg(args, char *);
+	if (str == NULL)
+		printf("(nil)");
+	if (str != NULL)
+		printf("%s", str);
+}
+
+/**
+ * print_double - that prints float
+ * @args: va_list
+ */
+void print_double(va_list args)
+{
+	double d;
+
+	d = va_arg(args, double);
+	printf("%f", d);
+}
+
+/**
+ * print_all - that prints anything
+ * * @format: variable const char *
  */
 void print_all(const char * const format, ...)
 {
-	ftype fa[] = {
-		{"c", tchar},
-		{"i", tint},
-		{"f", tfloat},
-		{"s", tstring}
-	};
-
-	int l1 = 0, l2 = 0;
-	va_list list;
+	va_list args;
 	char *comma = "";
+	char *comma2 = ", ";
+	int x = 0;
 
-	va_start(list, format);
-	while (format && format[l1])
+	va_start(args, format);
+
+	while (format[x] != '\0')
 	{
-		l2 = 0;
-		while (l2 < 4)
+		printf("%s", comma);
+		switch (format[x])
 		{
-			if (format[l1] == fa[l2].tc[0])
-			{
-				printf("%s", comma);
-				fa[l2].tf(list);
-				comma = ", ";
-			}
-			l2++;
+			case 'i':
+				print_int(args);
+				break;
+			case 'c':
+				print_char(args);
+				break;
+			case 's':
+				print_string(args);
+				break;
+			case 'f':
+				print_double(args);
+				break;
+			case 'e':
+				++x;
+				comma = "";
+				continue;
+			default:
+				break;
 		}
-		l1++;
+		comma = comma2;
+		++x;
 	}
 	printf("\n");
-	va_end(list);
+	va_end(args);
 }
