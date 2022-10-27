db811bc00f90b214057a9e068a46b0ce91c8ff7a
koolpedro <panyakorah@gmail.com>
2022-10-27 14:39:27 -0700

    task on project 0x14

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,13 +11,15 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int diff_bits = 0;
 	unsigned long int difference;
 
+	/* Xor both nums to show bit 1 if different bits */
 	difference = n ^ m;
 
+	/* keep shifting difference to right and tallying the ones up */
 	do {
 		diff_bits += (difference & 1);
 		difference >>= 1;
-	} while 
-		(difference > 0);
+	} while
+	(difference > 0);
 
 	return (diff_bits);
 }
